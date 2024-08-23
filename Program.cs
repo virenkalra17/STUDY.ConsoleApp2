e13d65978da2eed88ef08e2c09115891d9c30ecb
Viren Kalra <virenkalra966@gmail.com>
2024-08-23 09:59:53 +0530

    Made a file Simple

--- Program.cs
+++ Program.cs
@@ -1,6 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-<<<<<<< HEAD
-Console.WriteLine("Hello, World!, first change");
-=======
-Console.WriteLine("Hello, World!, first change, remote change, revert practice");
->>>>>>> 41ebc5bb5bc08099b4b7b2d153e8db9f16131450
+﻿Console.WriteLine("Simple");
